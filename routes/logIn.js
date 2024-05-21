690960aa4e008fb63738d9d3c244ab2bb5caf5b3
marijagaliatovic <92432581+marijagaliatovic@users.noreply.github.com>
2024-05-21 22:11:34 +0200

    logging out

--- routes/logIn.js
+++ routes/logIn.js
@@ -7,7 +7,7 @@ router.get('/', (req,res) => {
     console.log("req.session.passport: " + req.session.passport)
     console.log("Is user authenticated after authentication:", req.isAuthenticated());
     //res.redirect('login/profile')
-    res.send('Log')
+    res.send("req.session.passport: " + req.session.passport)
 })     
 
 router.post('/', passport.authenticate('local', {
