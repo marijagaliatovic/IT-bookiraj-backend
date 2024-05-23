18e2769587bfc1b327cc038ab8d317a779f88877
marijagaliatovic <92432581+marijagaliatovic@users.noreply.github.com>
2024-05-23 18:07:02 +0200

    logout

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors({
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     store: store,
     cookie: { maxAge: 30000 }
 }))
